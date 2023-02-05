0e0f7d1bc22899757f75689a6dc060e44747799c
Jhon <118701015+CalderonJh@users.noreply.github.com>
2023-02-05 01:06:53 -0500

    proyecto en Intellij

---
+++ Proyecto-Estructuras/src/Dijkstra.java
@@ -0,0 +1,78 @@
+import java.util.PriorityQueue;
+
+public class Dijkstra {
+
+    /**
+     * Java Record Class Node
+     * @param node identificador del vértice al que hace referencia el objeto
+     * @param dist distancia desde el vértice inicial hasta la del vértice que se referencia
+     */
+    private record Node(int node, int dist) implements Comparable<Node> {
+        //compara el dist del nodo actual con otro objeto node
+        public int compareTo(Node other) {
+            return Integer.compare(this.dist, other.dist);
+        }
+
+    }
+
+    /**
+     * Encuentra el camino más corto entre dos vertices específicos usando el algoritmo de Dijkstra
+     * @param graph grafo que contiene los vertices
+     * @param start vértice de partida
+     * @param end vértice de llegada
+     */
+    public static void shortestPath(Graph graph, int start, int end) {
+        int len = graph.getVertices();
+        int[][] adj = graph.getAdj();
+        int[] predecessor = new int[len];
+        int[] dist = new int[len];
+        boolean[] visited = new boolean[len];
+
+        //Queue para guardar ordenadamente según su distancia los nodos a visitar
+        PriorityQueue<Node> queue = new PriorityQueue<>();
+        for (int i = 0; i < len; i++) {
+            dist[i] = Integer.MAX_VALUE;
+            predecessor[i] = -1;
+        }
+        dist[start] = 0;
+        queue.add(new Node(start, 0));
+
+        while (!queue.isEmpty()) {
+            int curr = queue.poll().node();
+            if (!visited[curr]) {
+                visited[curr] = true;
+                for (int i = 0; i < len; i++) {
+                    if (adj[curr][i] != 0) {
+                        int newDist = dist[curr] + adj[curr][i];
+                        if (newDist < dist[i]) {
+                            dist[i] = newDist;
+                            queue.add(new Node(i, dist[i]));
+                            predecessor[i] = curr;
+                        }
+                    }
+                }
+            }
+            if (curr == end) {
+                break;
+            }
+        }
+
+        //imprime el camino más corto encontrado
+        System.out.println("The shortest path between the vertex " + start + " and " + end + ":");
+        System.out.println(start + " - " + end + ": " + dist[end]);
+        int curr = end;
+        while (curr != -1) {
+            System.out.print(curr + " ");
+            curr = predecessor[curr];
+        }
+    }
+
+    public static void allShortestPath(Graph graph, int start){
+        for (int i = 0; i < graph.getVertices(); i++) {
+            if(start == i) continue;
+            shortestPath(graph,start,i);
+            System.out.println();
+        }
+    }
+
+}
